a1ee5ad4ff413e08c4552cab73fc6559db9db4c4
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2014-10-07 18:58:25 +0100

    Moved the CMakeLists.txt to a new Build/ folder

---
+++ Build/CMakeFiles/Arrows.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Arrows.dir/home/sam/Final_Year_Project/src/main.cpp.o"
+  "Arrows.pdb"
+  "Arrows"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Arrows.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Build/CMakeFiles/Arrows.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Arrows.
+# This may be replaced when dependencies are built.
